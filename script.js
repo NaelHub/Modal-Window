0fabd2a1b1bfaef703f7c95ba6ec2e6bddaa0ee3
Nael Hubaishi <nael.hub@gmail.com>
2022-11-15 21:28:13 -0500

    Added Modal open/close functionality

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// close modal function
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+// open Modal function
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+// close Modal options - with exit but or with overlay
